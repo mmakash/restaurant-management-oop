1849b760c390a96ff4e6033afb3838c133d8b57f
akash <musaddekurakash@gmail.com>
2024-12-11 21:35:31 +0600

    employee class added

---
+++ user.py
@@ -0,0 +1,18 @@
+from abc import ABC
+
+class User(ABC):
+    def __init__(self,name,phone,email,address):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.address = address
+
+class Employee(User):
+    def __init__(self, name, phone, email, address,age,designation,salary):
+        super().__init__(name, phone, email, address)
+        self.age = age
+        self.designation = designation
+        self.salary = salary
+
+emp = Employee("rahim", "rahim@gmail.com", 1279787, "Dhaka", 23, "Chef", 12000)
+print(emp.name)
