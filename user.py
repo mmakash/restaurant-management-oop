d2b6761d510270f1d4290844d5fa9941399dceb9
akash <musaddekurakash@gmail.com>
2024-12-12 11:05:31 +0600

    Customer class added

--- user.py
+++ user.py
@@ -27,10 +27,36 @@ class Admin(User):
     def view_empoyees(self,restaurant):
         restaurant.view_empoyees()
 
+    def add_new_item(self,restaurant,item):
+        restaurant.menu.add_employee(item)
+        
+    def remove_item(self,restaurant,item):
+        restaurant.menu.remove_item(item)
+
+class Customer(User):
+    def __init__(self, name, phone, email, address):
+        super().__init__(name, phone, email, address)
+        self.cart = None
+    
+    def view_menu(self,restaurant):
+        restaurant.menu.show_menu()
+    
+    def add_to_cart(self,restaurant,item_name):
+        item = restaurant.menu.find_item(item_name)
+        if item:
+            pass
+        else:
+            print("item not found")
+    
+    def view_cart(self):
+        print("view cart")
+        print("Name\tPrice\tQuantity")
+
 class Restaurant:
     def __init__(self,name):
         self.name = name
         self.employees = [] #it would be our database
+        self.menu = FoodItem()
     
     def add_employee(self,employee):
         self.employees.append(employee)
@@ -60,8 +86,24 @@ class Menu:
             print("item deleted")
         else:
             print("item not found")
+    def show_menu(self):
+        print("***menu***")
+        print(f"Name\tPrice\tQuantity")
+        for item in self.items:
+            print(f"{item.name}\t{item.price}\t{item.quantity}")
 
-ad = Admin("ashik",1234,"aasd@gmail.com","bajitpur")
-ad.add_employee("sagor","sagor@gmail.com",464798,"rajibpur",23,"bussinessman",25000)
-ad.view_empoyees()
+class FoodItem:
+    def __init__(self,name,price,quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+
+# ad = Admin("ashik",1234,"aasd@gmail.com","bajitpur")
+# ad.add_employee("sagor","sagor@gmail.com",464798,"rajibpur",23,"bussinessman",25000)
+mn = Menu()
+item = FoodItem("pizza",12.45,10)
+mn.add_menu_itm(item)
+mn.show_menu()
+
 
